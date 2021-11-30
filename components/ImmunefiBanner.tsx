640b092dcc45955d9d796fdf127578ed8f5e4f9f
Charly Bernard <charly.bernard@gmail.com>
2021-11-30 19:37:09 +0800

    feat(immmunefiBanner): Bug Bouty - Add link in nav + component banner

---
+++ components/ImmunefiBanner.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  HStack,
+  Button,
+  Image,
+} from "@chakra-ui/react";
+
+import EdgeCornerIcon from "components/icons/EdgeCornerIcon";
+
+const ImmunefiBanner = () => {
+  return (
+    <Box
+      bg="linear-gradient(90deg, rgba(146,120,255,1) 0%, rgba(86,67,242,1) 100%);"
+      maxW="845px"
+      position="relative"
+      mt="24"
+    >
+      <EdgeCornerIcon
+        w="14px"
+        h="14px"
+        color="white"
+        position="absolute"
+        left="0"
+        top="0"
+        m="3"
+      />
+      <EdgeCornerIcon
+        w="14px"
+        h="14px"
+        color="white"
+        position="absolute"
+        right="0"
+        top="0"
+        transform="rotate(90deg)"
+        m="3"
+      />
+      <EdgeCornerIcon
+        w="14px"
+        h="14px"
+        color="white"
+        position="absolute"
+        left="0"
+        bottom="0"
+        transform="rotate(-90deg)"
+        m="3"
+      />
+      <EdgeCornerIcon
+        w="14px"
+        h="14px"
+        color="white"
+        position="absolute"
+        right="0"
+        bottom="0"
+        transform="rotate(180deg)"
+        m="3"
+      />
+      <Flex h="100%" justify="space-between" pt="6" px={[8, null, 20]}>
+        <Flex align="center" textAlign="left" pt="4">
+          <Box pb="8">
+            <Heading as="h2" color="#1AFAB7" fontWeight="400">
+              Get paid up to $1,000,000
+            </Heading>
+            <Text color="white" fontSize="1.25rem">
+              if you discover bugs and vulnerabilities on Astroport.
+            </Text>
+            <Box mt={[6, null, 7]} w={["60%", null, "auto"]}>
+              <HStack
+                spacing="0"
+                flexWrap="wrap"
+                my={[-3, null, -4]}
+                py={[3, null, 4]}
+              >
+                <Box py={[3, null, 4]} mr="4">
+                  <Button
+                    as="a"
+                    variant="primary"
+                    href="https://www.immunefi.com/bounty/astroport"
+                    size="sm"
+                    target="_blank"
+                    rel="noreferrer"
+                    bg="white"
+                    color="brand.purple"
+                    _hover={{ textDecoration: "underline" }}
+                  >
+                    Report Bug
+                  </Button>
+                </Box>
+                <HStack py={[3, null, 4]} spacing="4">
+                  <Text
+                    as="span"
+                    textTransform="uppercase"
+                    color="white"
+                    fontSize="0.8125rem"
+                  >
+                    with
+                  </Text>
+                  <Image
+                    src="/immunefi-logo.svg"
+                    alt="Immunefi"
+                    w={["92px", "109px", "127px"]}
+                  />
+                </HStack>
+              </HStack>
+            </Box>
+          </Box>
+        </Flex>
+        <Flex
+          pl="8"
+          align="self-end"
+          flexShrink={0}
+          w={["35%", "25%", "auto"]}
+          bottom="0"
+          right={["12", "20", "12"]}
+          position={["absolute", null, "static"]}
+        >
+          <Image src="/immunefi-find-bug-glass.svg" alt="" />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default ImmunefiBanner;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,8 +72,21 @@ const Navbar: FC = () => {
           <Box>
             <SocialsNav />
           </Box>
-          {/* <Box>
+          <Box>
             <Button
+              as="a"
+              variant="primary"
+              href="https://www.immunefi.com/bounty/astroport"
+              size="sm"
+              target="_blank"
+              rel="noreferrer"
+              bg="white"
+              color="brand.purple"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Bug Bounty
+            </Button>
+            {/* <Button
               as="a"
               variant="primary"
               href="/"
@@ -84,8 +97,8 @@ const Navbar: FC = () => {
               fontWeight="500"
             >
               Go to Astro drop
-            </Button>
-          </Box> */}
+            </Button> */}
+          </Box>
         </HStack>
         <Box display={{ md: "none" }} mr="-4">
           <Button variant="icon" ref={btnRef} onClick={onOpen}>
